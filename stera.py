ff38e0629f903b8a66a4dbe6e9c17d87ecd9eb24
Darlynuct <carladarlynuscamayta@gmail.com>
2021-10-12 21:53:22 -0400

    tarea_8_terminada

--- stera.py
+++ stera.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     response = requests.get('https://75jwlvujpd.execute-api.us-east-2.amazonaws.com/staging/expeditions/explore')
-    expedition = eval(response.text)
-#    message = "Sam dice que vayas a x: {}, y:{} el deposito es tiene este tamano:{}".format(expedition['x'], expedition['y'], expedition['tamano'])
+    expedition = eval(eval(response.text)['expedition'])
+    message = "Sam dice que vayas a x: {}, y:{} el deposito es tiene este tamano:{}".format(expedition['x'], expedition['y'], expedition['tamano'])
  
-    return "<p>Hola, {}</p>".format(expedition)
+    return "<p>Hola, {}</p>".format(message)
 
 
 if __name__ == '__main__':
